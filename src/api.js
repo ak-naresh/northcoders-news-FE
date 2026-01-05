750b4a2516000c185d07982454ef639c80187b67
AK Naresh <ak@AKs-MacBook-Pro-2.local>
2026-01-05 17:18:13 +0000

    Replace axios with native fetch API

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 //BE Render URL
-const BASE_URL = "https://aknaresh-ncnews.onrender.com/api";
+const API_URL = "https://aknaresh-ncnews.onrender.com/api";
 
-export const getArticles = () => {
-  return fetch(`${BASE_URL}/articles`)
+const getArticles = () => {
+  return fetch(`${API_URL}/articles`)
     .then((response) => {
       return response.json();
     })
@@ -10,3 +10,5 @@ export const getArticles = () => {
       return data.articles;
     });
 };
+
+export { getArticles };
